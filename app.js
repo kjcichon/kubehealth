bc081832cda27f53ed9e49616ce828fbb37393fa
Kevin Cichon <kcichon@follett.com>
2016-11-04 15:07:45 -0500

    health

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 var express = require('express')
 var app = express()
+var session = require("express-session");
+
+app.use(session({resave: true, saveUninitialized: true, secret: 'SOMERANDOMSECRETHERE', cookie: { maxAge: 60000 }}));
+
+app.get('/kill', function (req, res) {
+  session.kill='yes';
+  res.send('I am not doing so well')
+})
+
+app.get('/alive', function (req, res) {
+  session.kill='no';
+  res.send('Back to life')
+})
 
 app.get('/healthz', function (req, res) {
-  res.send('I am health')
+  var kill = session.kill;
+  if (kill==='yes'){
+    res.status(404)        // HTTP status 404: NotFound
+        .send('Not found');
+  } else {
+    res.status(200)
+        .send('All Good');
+  }
 })
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
