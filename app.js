9fa1c150f5612556c3104a37708bcfa0761bf027
Kevin Cichon <kcichon@follett.com>
2016-11-07 08:48:00 -0600

    Added 2 probes

--- app.js
+++ app.js
@@ -4,24 +4,51 @@ var session = require("express-session");
 
 app.use(session({resave: true, saveUninitialized: true, secret: 'SOMERANDOMSECRETHERE', cookie: { maxAge: 60000 }}));
 
-app.get('/kill', function (req, res) {
-  session.kill='yes';
-  res.send('I am not doing so well')
+app.get('/unready', function (req, res) {
+  session.ready='no';
+  console.log("not ready");
+  res.send('I am not ready yet');
+})
+
+app.get('/ready', function (req, res) {
+  session.ready='yes';
+  console.log("Setting ready to yes");
+  res.send('Ready fredy');
+})
+
+app.get('/isready', function (req, res) {
+  var ready = session.ready;
+  console.log("hitting ready probe=" + ready)
+  if (ready==='no'){
+    res.status(404)        // HTTP status 404: NotFound
+        .send('Not ready');
+  } else {
+    res.status(200)
+        .send('ready');
+  }
+})
+
+app.get('/dead', function (req, res) {
+  session.alive='no';
+  console.log("dead");
+  res.send('I am undead');
 })
 
 app.get('/alive', function (req, res) {
-  session.kill='no';
-  res.send('Back to life')
+  session.alive='yes';
+  console.log("alive");
+  res.send('alive');
 })
 
-app.get('/healthz', function (req, res) {
-  var kill = session.kill;
-  if (kill==='yes'){
+pp.get('/islively', function (req, res) {
+  var alive = session.alive;
+  console.log("hitting lively probe=" + alive)
+  if (alive==='no'){
     res.status(404)        // HTTP status 404: NotFound
-        .send('Not found');
+        .send('not alive');
   } else {
     res.status(200)
-        .send('All Good');
+        .send('breathing');
   }
 })
 
@@ -30,6 +57,6 @@ app.get('/', function (req, res) {
 })
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
 })
